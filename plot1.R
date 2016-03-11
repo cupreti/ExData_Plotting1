da7940838385b45abff2eefacb821b5364883c9f
cupreti <cupreti@veriskhealth.com>
2016-03-11 15:22:36 +0545

    Added R Code

--- plot1.R
+++ plot1.R
@@ -1,1 +1,14 @@
 # This script creates plot1 graph
+
+# load Data
+source("DataSource.R")
+
+# creates plot1
+plot1 <- function() {
+  png("plot1.png", width = 500, height = 500)
+  hist(
+    as.numeric(subSetData$Global_active_power), col = "red", main = "Global Active Power", xlab =
+      "Global Active Power (kilowatts)"
+  )
+  dev.off()
+}
--- plot2.R
+++ plot2.R
@@ -1,2 +1,17 @@
-datafile <- "./exdata-data-household_power_consumption/household_power_consumption.txt"
-data <- read.table(datafile,header = TRUE, sep = ";",stringsAsFactors = FALSE, dec = ".")
+# This script creates plot2 graph
+
+# load Data
+source("DataSource.R")
+
+# creates plot2
+plot2 <- function() {
+  datetime <-
+    strptime(paste(subSetData$Date, subSetData$Time, sep = " "), "%d/%m/%Y %H:%M:%S")
+  png("plot2.png", width = 500, height = 500)
+  plot(
+    datetime, as.numeric(subSetData$Global_active_power), type = "l", xlab =
+      "", ylab = "Global Active Power (kilowatts)"
+  )
+  dev.off()
+}
+
